04f8fbd40ae2fdb53950d267812024b9edb67c1c
akzana <akzanaklepsch@outlook.com>
2024-11-21 15:37:33 -0800

    standardize styles and folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ""
 import './App.css'
+import WelcomeComponent from './components/1-WelcomeComponent/WelcomeComponent'
+import IntroToAIComponent from './components/2-IntroToAIComponent/IntroToAIComponent'
+import UseAIComponent from './components/3-UseAIComponent/UseAIComponent'
+import AIProcessesComponent from './components/4-AIProcessesComponent/AIProcessesComponent'
+import LearnMoreComponent from "./components/5-LearnMoreComponent"
+import CourseFeedbackComponent from './components/6-CourseFeedbackComponent/CourseFeedbackComponent'
 
 function App() {
 
@@ -9,7 +13,12 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage/>} />
-        <Route path="/course" element={<CoursePage/>} />
+        <Route path="/welcome" element={<WelcomeComponent/>} />
+        <Route path="/intro" element={<IntroToAIComponent/>} />
+        <Route path="/aiuse" element={<UseAIComponent/>} />
+        <Route path="/aiprocesses" element={<AIProcessesComponent/>} />
+        <Route path="/learnmore" element={<LearnMoreComponent/>} />
+        <Route path="/feedback" element={<CourseFeedbackComponent/>} />
       </Routes>
     </BrowserRouter>
   )
