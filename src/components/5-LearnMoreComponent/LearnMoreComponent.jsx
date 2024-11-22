06ec84296523fbd30adf37c8f69b73b16e696d06
akzana <akzanaklepsch@outlook.com>
2024-11-21 18:12:31 -0800

    add learn more content

--- src/components/5-LearnMoreComponent/LearnMoreComponent.jsx
+++ src/components/5-LearnMoreComponent/LearnMoreComponent.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import ComponentHeader from '../0-ComponentHeader/ComponentHeader'
 
-export default function UsingAIOnFBComponent() {
+export default function LearnMoreComponent() {
   return (
     <div>
+      <ComponentHeader/>
+      <h1 className="heading">Learn More</h1>
+      <h3 className="learnmore">Check Out These Courses & Get Involved</h3>
       
     </div>
   )
