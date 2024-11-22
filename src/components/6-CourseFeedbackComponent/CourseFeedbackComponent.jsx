4f932d6d2f3d551f999ece22802764bdcbc09956
Jenny Ho <nha.jennyho@gmail.com>
2024-11-21 23:29:48 -0500

    fill out form questions

--- src/components/6-CourseFeedbackComponent/CourseFeedbackComponent.jsx
+++ src/components/6-CourseFeedbackComponent/CourseFeedbackComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import ComponentHeader from '../0-ComponentHeader/ComponentHeader';
+import './CourseFeedbackComponent.scss'
 
 export default function CourseFeedbackComponent() {
   const navigate = useNavigate();
@@ -26,34 +27,64 @@ export default function CourseFeedbackComponent() {
         showBack={true} 
         showNext={false} 
       />
-      <div>
-      <h1>We'd love your feedback!</h1>
-      <h2>Your feedback helps us improve and create more engaging experiences</h2>
-      <form>
-        <div>
-        {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
-          <label>Rate your overall experience of the course
+      <div className="survey">
+      <h1 className="survey__title">We'd love your feedback!</h1>
+      <h2 className="survey__subtitle">Your feedback helps us improve and create more engaging experiences</h2>
+      <form className="survey-form"> 
+        <div className="survey-form__rateExperience">
+          <p className="survey-form__label">Rate your overall experience of the course</p>
+          {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
+          <label key={num} for="question-one"> {num}
             <input
               type="radio"
-              name="rateExperience"
+              name="question-one"
+              id="question-one"
+              value={num}
+              onChange={(e) => setRating(e.target.value)}
+              required
             ></input>
           </label> 
         ))}
         </div>
-       
-        
-        <label>Rate your experience with navigating the course pages </label>
-        <label>What did you learn about AI that surprised you?
+        <div className="survey-form__rateNavigate">
+          <p>Rate your experience with navigating the course pages</p>
+          {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
+          <label key={num} for="question-two"> {num}
+            <input
+              type="radio"
+              name="question-two"
+              id="question-two"
+              value={num}
+              onChange={(e) => setRating(e.target.value)}
+              required
+            ></input>
+          </label> 
+        ))}
+        </div>
+        <label for="question-three" className="survey-form__label">What did you learn about AI that surprised you?
         </label>
         <input
-          type="radio"
-            name="rateNavigate"
+          type="text"
+          name="question-three"
+          id="question-three"
+          placeholder="Please enter your answer here"
         ></input>
-        <label>What features of AI do you want to explore further?
+        <label for="question-four" className="survey-form__label">What features of AI do you want to explore further?
         </label>
-        <input></input>
-        <label>How can we make this course better?</label>
-        <button></button>
+        <input
+          type="text"
+          name="question-four"
+          id="question-four"
+          placeholder="Please enter your answer here"
+        ></input>
+        <label for="question-five" className="survey-form__label">How can we make this course better?</label>
+        <input
+          type="text"
+          name="question-five"
+          id="question-five"
+          placeholder="Please enter your answer here"
+          ></input>
+        <button>Submit your survey</button>
       </form>
       </div>
       
