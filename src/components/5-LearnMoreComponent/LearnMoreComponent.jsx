cfa3a2098b9612c7eceb05a8f712273c9834e260
akzana <akzanaklepsch@outlook.com>
2024-11-21 19:27:05 -0800

    add list of meta ai products

--- src/components/5-LearnMoreComponent/LearnMoreComponent.jsx
+++ src/components/5-LearnMoreComponent/LearnMoreComponent.jsx
@@ -18,6 +18,24 @@ export default function LearnMoreComponent() {
           Advanced AI Use for FaceBook
         </li>
       </ul>
+      <h3 className="learnmore">Try Out Other AI From Meta</h3>
+      <ul className="course-list-2">
+        <li className="course-item">
+          Want to hear your voice in another language? Try Language AI
+        </li>
+        <li className="course-item">
+          Want bring your sketches to life with animations? Try Vision AI
+        </li>
+        <li className="course-item">
+          Want to create your own moive? Try Movie Gen
+        </li>
+        <li className="course-item">
+          Want to create your own AI bot? Try AIStudio
+        </li>
+        <li className="course-item">
+          Want to write a persuasive blog? Try Meta AI
+        </li>
+      </ul>
     </div>
   )
 }
