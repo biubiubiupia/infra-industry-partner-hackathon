ba3afc14eeb5889f5418eb6bead4f0dd7d500174
akzana <akzanaklepsch@outlook.com>
2024-11-21 18:59:40 -0800

    add content

--- src/components/5-LearnMoreComponent/LearnMoreComponent.jsx
+++ src/components/5-LearnMoreComponent/LearnMoreComponent.jsx
@@ -4,12 +4,18 @@ import ComponentHeader from '../0-ComponentHeader/ComponentHeader'
 export default function LearnMoreComponent() {
   return (
     <div>
-      <ComponentHeader/>
+      <ComponentHeader />
       <h1 className="heading">Learn More</h1>
       <h3 className="learnmore">Check Out These Courses & Get Involved</h3>
       <ul className="course-list">
         <li className="course-item">
-          
+          Responsible AI Use & Ethics
+        </li>
+        <li className="course-item">
+          Boost Your Creativity with Generative AI
+        </li>
+        <li className="course-item">
+          Advanced AI Use for FaceBook
         </li>
       </ul>
     </div>
