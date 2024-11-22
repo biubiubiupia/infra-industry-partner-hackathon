483911ac7de4a34a389ab15a38982dc18678bf3c
akzana <akzanaklepsch@outlook.com>
2024-11-21 18:46:20 -0800

    add fontface to app

--- src/components/5-LearnMoreComponent/LearnMoreComponent.jsx
+++ src/components/5-LearnMoreComponent/LearnMoreComponent.jsx
@@ -7,7 +7,11 @@ export default function LearnMoreComponent() {
       <ComponentHeader/>
       <h1 className="heading">Learn More</h1>
       <h3 className="learnmore">Check Out These Courses & Get Involved</h3>
-      
+      <ul className="course-list">
+        <li className="course-item">
+          
+        </li>
+      </ul>
     </div>
   )
 }
