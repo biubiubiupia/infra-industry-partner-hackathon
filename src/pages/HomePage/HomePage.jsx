7e5f74cb9775369aea51f4dcbfb201055f311dc1
Tina Feng <contact@tinafeng.com>
2024-11-22 11:42:24 -0500

    add second button to post area

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,6 +7,17 @@ export default function HomePage() {
 
   return (
     <main className="homepage">
+        <div className="post">
+        <NavLink to="/welcome">
+            <button className="prompt2">
+            <img src={AILogo} alt="Meta AI Logo" className="prompt2__logo" />
+            <p className="prompt2__text">
+              Explore<br></br>Meta AI
+            </p>
+          </button>
+          </NavLink>
+        </div>
+
         <div className="messenger">
           <NavLink to="/welcome">
             <button className="prompt">
