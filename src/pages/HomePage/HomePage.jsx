d0aad06015acab8f56f13f2007330221b84f7979
Tina Feng <contact@tinafeng.com>
2024-11-22 11:50:10 -0500

    add the third button

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,30 +4,39 @@ import { NavLink } from "react-router-dom";
 import AILogo from "../../assets/logos/meta-ai-logo.png";
 
 export default function HomePage() {
-
   return (
     <main className="homepage">
-        <div className="post">
+      <div className="ads">
         <NavLink to="/welcome">
-            <button className="prompt2">
+          <button className="prompt3">
+            <img src={AILogo} alt="Meta AI Logo" className="prompt3__logo" />
+            <p className="prompt3__text">
+              Explore<br></br>Meta AI
+            </p>
+          </button>
+        </NavLink>
+      </div>
+      <div className="post">
+        <NavLink to="/welcome">
+          <button className="prompt2">
             <img src={AILogo} alt="Meta AI Logo" className="prompt2__logo" />
             <p className="prompt2__text">
               Explore<br></br>Meta AI
             </p>
           </button>
-          </NavLink>
-        </div>
+        </NavLink>
+      </div>
 
-        <div className="messenger">
-          <NavLink to="/welcome">
-            <button className="prompt">
+      <div className="messenger">
+        <NavLink to="/welcome">
+          <button className="prompt">
             <img src={AILogo} alt="Meta AI Logo" className="prompt__logo" />
             <p className="prompt__text">
               Explore<br></br>Meta AI
             </p>
           </button>
-          </NavLink>
-        </div>
+        </NavLink>
+      </div>
     </main>
   );
 }
