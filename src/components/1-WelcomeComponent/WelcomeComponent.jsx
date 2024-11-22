e2371ea1de483a423f22a84efb9f5777f8435c4d
Roshani Faleel <roshfaleel98@gmail.com>
2024-11-22 12:50:48 -0500

    add the components and stylings

--- src/components/1-WelcomeComponent/WelcomeComponent.jsx
+++ src/components/1-WelcomeComponent/WelcomeComponent.jsx
@@ -1,39 +1,66 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import ComponentHeader from '../0-ComponentHeader/ComponentHeader';
-import './WelcomeComponent.scss';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import ComponentHeader from "../0-ComponentHeader/ComponentHeader";
+import "./WelcomeComponent.scss";
 
 export default function WelcomeComponent() {
   const navigate = useNavigate();
 
   const handleClose = () => {
-    navigate('/');
+    navigate("/");
   };
 
   const handleBack = () => {
-    navigate('/welcome');
+    navigate("/welcome");
   };
 
   const handleNext = () => {
-    navigate('/intro');
+    navigate("/intro");
   };
 
   return (
-    <div className="welcome-container">
-      <div className="component-card">
-        <ComponentHeader
-          onClose={handleClose}
-          onBack={handleBack}
-          onNext={handleNext}
-          showBack={true}
-          showNext={true}
-        >
-          <h1>Welcome Component</h1>
-          
-        </ComponentHeader>
+    <div className="page">
+      <div className="welcome-container">
+        <div className="component-card">
+          <ComponentHeader
+            onClose={handleClose}
+            onBack={handleBack}
+            onNext={handleNext}
+            showBack={true}
+            showNext={true}
+          >
+            <section className="welcome-card">
+              <h1 className="welcome-card__header">Step Into the World of AI</h1>
+              <p className="welcome-card__details">
+                Discover how Artificial Intelligence (AI) is transforming the
+                way you connect, share, and experience the digital world. Get
+                ready to uncover the magic behind the tools that power your
+                Facebook journey!
+              </p>
+
+              <h2 className="welcome-card__subheader" >What Awaits You?</h2>
+              <div className="welcome-card__container">
+              <div className="welcome-card__card">
+                <img  className="welcome-card__img" src="src/assets/images/welcome_1.jpg"/>
+              <h3 className="welcome-card__card-title">Unlock the Basics of AI</h3>
+              </div>
+              <div className="welcome-card__card">
+                <img className="welcome-card__img" src="src/assets/images/welcome_2.jpg"/>
+              <h3 className="welcome-card__card-title">Dive Into the Tech Behind the Magic</h3>
+              </div>
+              <div className="welcome-card__card">
+              <img className="welcome-card__img" src="src/assets/images/welcome_3.jpg"/>
+              <h3 className="welcome-card__card-title">See How Meta’s AI Enhances Your Everyday Life</h3>
+              </div>
+              <div className="welcome-card__card">
+              <img className="welcome-card__img" src="src/assets/images/welcome_4.jpg"/>
+              <h3 className="welcome-card__card-title"> Start Your Journey to Master AI Today!</h3>
+              </div>
+              </div>
+            </section>
+          </ComponentHeader>
+        </div>
       </div>
     </div>
   );
 }
-
-
