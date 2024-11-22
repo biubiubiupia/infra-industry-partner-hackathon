1133cbdef17fd137468212129f0ba681788a94f0
Tina Feng <contact@tinafeng.com>
2024-11-21 22:21:09 -0500

    create button component and styling

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,18 @@
 import "./HomePage.scss"
 import React from 'react'
+import AILogo from "../../assets/"
 
 export default function HomePage() {
   return (
-    <div className="page">
-     <h1>HELLO</h1>
-    </div>
+    <main className="page">
+      <div className="page__overlay">
+        <div className="messenger">
+          <button className="prompt">
+            <span><AILogo /></span>
+            <span className="button-text">Meta AI</span>
+          </button>
+        </div>
+      </div>
+    </main>
   )
 }
