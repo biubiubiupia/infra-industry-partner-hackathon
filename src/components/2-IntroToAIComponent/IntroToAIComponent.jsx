41663027a8e9e4509e822a5fd7d23e3c548cdd84
Tina Feng <contact@tinafeng.com>
2024-11-22 13:24:56 -0500

    more styling for Intro to AI

--- src/components/2-IntroToAIComponent/IntroToAIComponent.jsx
+++ src/components/2-IntroToAIComponent/IntroToAIComponent.jsx
@@ -34,6 +34,10 @@ export default function IntroToAIComponent() {
               <div className="intro__box">
                 <div className="intro__card">
                   <h3 className="intro__subheader">Definition of AI </h3>
+                  <img
+                    className="intro__image"
+                    src="https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?t=st=1732298966~exp=1732302566~hmac=8f2e9dfe49125462b8912f72347e9c8ea4dd8b801f1df3311d0b343bfb371ad7&w=1480"
+                  ></img>
                   <p className="intro__paragrah">
                     Artificial Intelligence (AI) refers to computer systems that
                     can perform tasks that typically require human intelligence,
@@ -42,15 +46,23 @@ export default function IntroToAIComponent() {
                 </div>
                 <div className="intro__card">
                   <h3 className="intro__subheader">Types of AI</h3>
+                   <img
+                    className="intro__image"
+                    src="https://media.wired.com/photos/64125850412651fa6ee1479c/master/pass/AI-Powered-Future-of-Sound-Culture-1439768811.jpg"
+                  ></img>
                   <p className="intro__paragrah">
-                    There are two primary types of AI:<br></br>Narrow AI (focused on a
-                    specific task) and General AI (capable of performing any
-                    intellectual task). Most AI systems used today are Narrow
-                    AI.
+                    There are two primary types of AI:<br></br>Narrow AI
+                    (focused on a specific task) and General AI (capable of
+                    performing any intellectual task). Most AI systems used
+                    today are Narrow AI.
                   </p>
                 </div>
                 <div className="intro__card">
                   <h3 className="intro__subheader">How AI Works</h3>
+                  <img
+                    className="intro__image"
+                    src="https://leonfurze.com/wp-content/uploads/2023/09/purple_pink_blue_wave_forms_synthwave_audio_black_ba_6b55753c-f62c-48d2-9c4f-7c90c06c6296.png"
+                  ></img>
                   <p className="intro__paragrah">
                     AI systems rely on algorithms, data, and computing power to
                     learn from experience and improve their performance over
@@ -59,6 +71,10 @@ export default function IntroToAIComponent() {
                 </div>
                 <div className="intro__card">
                   <h3 className="intro__subheader">AI Applications</h3>
+                  <img
+                    className="intro__image"
+                    src="https://img.freepik.com/premium-vector/ai-technology-concept-artificial-intelligence-blue-purple-dots-lines-digital-communication-science-networks_322958-3322.jpg"
+                  ></img>
                   <p className="intro__paragrah">
                     AI is used in various applications, including virtual
                     assistants, image recognition, natural language processing,
